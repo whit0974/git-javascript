94b39e016e15ac9d87edcdcb9ef93570bb45c270
whitehurst <whit0974@algonquinlive.com>
2019-09-10 13:00:18 -0400

    project ok

---
+++ script.js
@@ -0,0 +1,16 @@
+var myName = 'Kelly Whitehurst de Castro'
+myName = 'Kelly'
+console.log('Hello my name is')
+console.log('10')
+function multiply(10,2){
+    return 10*2
+}
+function multiply(20,5){
+    return 20*5
+}
+function divide(100,10){
+    return 100/10
+}
+function subtract(10,7){
+    return 10-7
+}
